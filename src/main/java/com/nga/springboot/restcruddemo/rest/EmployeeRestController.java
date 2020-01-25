f395e0057b167a507c4bce44baf5250e91388570
Grigory <ovonely@gmail.com>
2020-01-25 23:27:50 +0700

    Spring Boot - REST CRUD:   - adding REST Controller methods to find and add employee

--- src/main/java/com/nga/springboot/restcruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/nga/springboot/restcruddemo/rest/EmployeeRestController.java
@@ -4,9 +4,7 @@ import com.nga.springboot.restcruddemo.dao.EmployeeDAO;
 import com.nga.springboot.restcruddemo.entity.Employee;
 import com.nga.springboot.restcruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,4 +25,31 @@ public class EmployeeRestController {
     public List<Employee> findAll() {
         return employeeService.findAll();
     }
+
+    // add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found = " + employeeId);
+        }
+
+        return employee;
+    }
+
+    // add mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+
+        // also just in case they pass an id in JSON - set id to 0
+        // this is to force a save of new item
+        employee.setId(0);
+
+        employeeService.save(employee);
+
+        return employee;
+    }
+
 }
