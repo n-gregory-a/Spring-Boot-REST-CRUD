c230ad7edcb5d74f4289fc32f0bc59c13a170024
Grigory <ovonely@gmail.com>
2020-01-25 23:45:50 +0700

    Spring Boot - REST CRUD:   - adding REST Controller methods to update and delete employee

--- src/main/java/com/nga/springboot/restcruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/nga/springboot/restcruddemo/rest/EmployeeRestController.java
@@ -52,4 +52,30 @@ public class EmployeeRestController {
         return employee;
     }
 
+    // add mapping for PUT /employees - update existing employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+
+        employeeService.save(employee);
+
+        return employee;
+
+    }
+
+    // add mapping for DELETE /employees/{employeeId}
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        Employee employee = employeeService.findById(employeeId);
+
+        // throw exception if null
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+    }
+
 }
