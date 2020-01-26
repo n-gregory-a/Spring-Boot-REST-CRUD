06d051c6d2cea0988f9fd53c575988d88daf22d9
Grigory <ovonely@gmail.com>
2020-01-26 22:27:44 +0700

    Spring Boot - REST CRUD - Spring Data JPA:   - import optimization in EmployeeServiceImpl

--- src/main/java/com/nga/springboot/restcruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/nga/springboot/restcruddemo/service/EmployeeServiceImpl.java
@@ -4,7 +4,6 @@ import com.nga.springboot.restcruddemo.dao.EmployeeRepository;
 import com.nga.springboot.restcruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
