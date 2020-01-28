d93c6d46629888582452b7ab346311291137f578
Grigory <ovonely@gmail.com>
2020-01-28 22:24:33 +0700

    Spring Boot - Spring Data REST - configuration, pagination, sorting:   - changing REST resource path   - changing the default page size

--- src/main/java/com/nga/springboot/restcruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/nga/springboot/restcruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.nga.springboot.restcruddemo.dao;
 
 import com.nga.springboot.restcruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // no need to write any code here!!!
 }
